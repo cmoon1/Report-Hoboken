9dcf7658aa2e24c328fa97086f2e40a72416971f
Christopher Moon <cmoon1@stevens.edu>
2022-04-11 20:26:37 -0400

    Added pagination to issues list

--- client/src/Components/MapComponent.js
+++ client/src/Components/MapComponent.js
@@ -1,22 +1,20 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
 import {
 	Typography,
-	Grid,
-	Card,
-	CardContent,
 	List,
 	ListItem,
 	ListItemText,
 	Divider,
 	ListItemButton,
+	Pagination,
 } from "@mui/material";
 function MapComponent() {
-	const center = useMemo(() => ({ lat: 40.744, lng: -74.0324 }), []);
-	const [selectedElement, setSelectedElement] = useState(null);
+	const [center, setCenter] = useState({ lat: 40.744, lng: -74.0324 });
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(3);
 	const [activeMarker, setActiveMarker] = useState(null);
-	const [showInfoWindow, setInfoWindowFlag] = useState(false);
-
+	const [selectedIndex, setSelectedIndex] = useState(null);
 	const [issuesData, setIssuesData] = useState([
 		{
 			title: "Water issue",
@@ -31,14 +29,59 @@ function MapComponent() {
 			name: "Billy Bob",
 		},
 		{
+			title: "Air Quality",
+			location: "Reported at 100 Ave",
+			coords: { lat: 40.7445, lng: -74.0324 },
+			name: "Chris Moon",
+		},
+		{
+			title: "Potholes",
+			location: "Reported at 235 Road",
+			coords: { lat: 40.7442, lng: -74.026 },
+			name: "David Smith",
+		},
+		{
+			title: "Power issue",
+			location: "Reported at somewhere",
+			coords: { lat: 40.746, lng: -74.03 },
+			name: "John Smith",
+		},
+		{
+			title: "Noise issue",
+			location: "Reported at Stevens",
+			coords: { lat: 40.7433, lng: -74.0266 },
+			name: "Anonymous",
+		},
+		{
+			title: "Construction",
+			location: "Reported at 1 Ave",
+			coords: { lat: 40.742, lng: -74.031 },
+			name: "Bobby Jones",
+		},
+		{
+			title: "Police Activity",
+			location: "Reported at 101 Street",
+			coords: { lat: 40.745, lng: -74.026 },
+			name: "Billy Bob",
+		},
+		{
 			title: "Power issue",
 			location: "Reported at 1000 Ave",
-			coords: { lat: 40.7445, lng: -74.0324 },
-			name: "Billy Bob",
+			coords: { lat: 40.746, lng: -74.028 },
+			name: "Billy Jones",
 		},
 	]);
 	let issuesList = [];
-	let markersList = [];
+
+	const handleListItemClick = (event, index) => {
+		setSelectedIndex(index);
+		setActiveMarker(index);
+		setCenter(issuesData[index].coords);
+	};
+
+	const handlePageChange = (e, value) => {
+		setCurrentPage(value);
+	};
 
 	const handleToggleOpen = (markerId) => {
 		setActiveMarker(markerId);
@@ -50,49 +93,42 @@ function MapComponent() {
 
 	const buildCards = (issue, index) => {
 		return (
-			<ListItem alignItems="flex-start" key={index}>
-				<ListItemButton>
-					<ListItemText
-						primary={issue.title}
-						secondary={
-							<React.Fragment>
-								<Typography
-									sx={{ display: "inline" }}
-									component="span"
-									variant="body2"
-									color="text.primary"
-								>
-									{issue.name}
-								</Typography>
-								{" - " + issue.location}
-							</React.Fragment>
-						}
-					/>
-				</ListItemButton>
-			</ListItem>
+			<div key={index}>
+				<ListItem alignItems="flex-start">
+					<ListItemButton
+						selected={selectedIndex === index}
+						onClick={(event) => handleListItemClick(event, index)}
+					>
+						<ListItemText
+							primary={issue.title}
+							secondary={
+								<React.Fragment>
+									<Typography
+										sx={{ display: "inline" }}
+										component="span"
+										variant="body2"
+										color="text.primary"
+									>
+										{issue.name}
+									</Typography>
+									{" - " + issue.location}
+								</React.Fragment>
+							}
+						/>
+					</ListItemButton>
+				</ListItem>
+				<Divider variant="insert" component="li" />
+			</div>
 		);
 	};
 
-	const buildMarkers = (issue, index) => {
-		return (
-			<Marker
-				key={index}
-				position={{ lat: issue.coords.lat, lng: issue.coords.lng }}
-			/>
-		);
-	};
-	console.log(activeMarker);
 	issuesList =
 		issuesData &&
-		issuesData.map((issue, index) => {
-			return buildCards(issue, index);
-		});
-
-	markersList =
-		issuesData &&
-		issuesData.map((issue, index) => {
-			return buildMarkers(issue, index);
-		});
+		issuesData
+			.slice((currentPage - 1) * 5, currentPage * 5)
+			.map((issue, index) => {
+				return buildCards(issue, index);
+			});
 
 	return (
 		<div style={{ display: "flex" }}>
@@ -100,40 +136,38 @@ function MapComponent() {
 				zoom={15}
 				center={center}
 				mapContainerClassName="map-container"
-				// onLoad={(map) => {
-				// 	for (let i = 0; i < 3; i++) {
-				// 		new window.google.maps.Marker({
-				// 			position: issuesData[i].coords,
-				// 			map,
-				// 			title: "Hello!",
-				// 		});
-				// 	}
-				// }}
 			>
-				{issuesData.map((issue, index) => {
-					return (
-						<div key={index}>
-							<Marker
-								title={issue.title}
-								position={{ lat: issue.coords.lat, lng: issue.coords.lng }}
-								onClick={() => handleToggleOpen(index)}
-							/>
-							{activeMarker === index ? (
-								<InfoWindow
-									key={index + issue.title}
-									onCloseClick={handleToggleClose}
-									position={{ lat: issue.coords.lat, lng: issue.coords.lng }}
-								>
-									<div>
-										<h2>{issue.title}</h2>
-										<br />
-										<p>{issue.location}</p>
-									</div>
-								</InfoWindow>
-							) : null}
-						</div>
-					);
-				})}
+				{issuesData &&
+					issuesData
+						.slice((currentPage - 1) * 5, currentPage * 5)
+						.map((issue, index) => {
+							return (
+								<div key={index}>
+									<Marker
+										title={issue.title}
+										position={{ lat: issue.coords.lat, lng: issue.coords.lng }}
+										onClick={() => handleToggleOpen(index)}
+										animation={window.google.maps.Animation.DROP}
+									/>
+									{activeMarker === index ? (
+										<InfoWindow
+											key={index + issue.title}
+											onCloseClick={handleToggleClose}
+											position={{
+												lat: issue.coords.lat,
+												lng: issue.coords.lng,
+											}}
+										>
+											<div>
+												<h2>{issue.title}</h2>
+												<br />
+												<p>{issue.location}</p>
+											</div>
+										</InfoWindow>
+									) : null}
+								</div>
+							);
+						})}
 			</GoogleMap>
 			{/* <div className="map-container"></div> */}
 
@@ -141,10 +175,20 @@ function MapComponent() {
 				<h1 style={{ textAlign: "center", justifyContent: "center" }}>
 					Current Issues
 				</h1>
-				{/* <Grid container>{issuesList}</Grid> */}
 				<List sx={{ width: "100%", bgcolor: "background.paper" }}>
+					<Divider variant="insert" component="li" />
 					{issuesList}
 				</List>
+				<Pagination
+					count={totalPages}
+					page={currentPage}
+					onChange={handlePageChange}
+					defaultPage={1}
+					color="primary"
+					size="large"
+					showFirstButton
+					showLastButton
+				/>
 			</div>
 		</div>
 	);
